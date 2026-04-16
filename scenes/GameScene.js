b10003bef01c555b54bf0bd7288a0fa5169ff717
simon245 <simon245@tlu.ee>
2026-04-16 16:58:46 +0300

    GameScene
    initial GameScene

--- scenes/GameScene.js
+++ scenes/GameScene.js
@@ -1,25 +1,12 @@
-import Player from "../entities/Player.js";
-
 export default class GameScene extends Phaser.Scene {
-
     constructor() {
         super("GameScene");
     }
 
     create() {
-	
-        this.player = new Player(this);
-        this.cursors = this.input.keyboard.createCursorKeys();
-    }
-
-    update() {
-
-        if (this.cursors.left.isDown) {
-            this.player.moveLeft();
-        }
-
-        if (this.cursors.right.isDown) {
-            this.player.moveRight();
-        }
+        this.add.text(100, 250, "Who Gets the Bed?", {
+            fontSize: "20px",
+            color: "#000"
+        });
     }
 }
