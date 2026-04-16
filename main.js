0b164b7cbc739aeb13cf4570a04bf3f68130206e
simon245 <simon245@tlu.ee>
2026-04-16 16:42:33 +0300

    setup
    setup Phaser project with main scenes

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 import GameScene from "./scenes/GameScene.js";
+import ResultScene from "./scenes/ResultScene.js";
 
 const config = {
     type: Phaser.AUTO,
     width: 400,
     height: 600,
-    backgroundColor: "#222",
-    scene: [GameScene]
+    backgroundColor: "#f5f5f5",
+    scene: [GameScene, ResultScene]
 };
 
 new Phaser.Game(config);
