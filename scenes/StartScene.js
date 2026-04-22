9a23f959a18719a142affb976ae539285e155dea
simon245 <simon245@tlu.ee>
2026-04-22 15:26:19 +0300

    test StartScene

--- scenes/StartScene.js
+++ scenes/StartScene.js
@@ -6,19 +6,19 @@ export default class StartScene extends Phaser.Scene {
     create() {
         this.cameras.main.setBackgroundColor("#ffffff");
 
-        this.add.text(100, 150, "Doctor On Duty", {
+        this.add.text(80, 150, "Doctor On Duty", {
             fontSize: "28px",
             color: "#000"
         });
 
-        const startBtn = this.add.text(120, 300, "START", {
+        const startBtn = this.add.text(140, 300, "START", {
             fontSize: "24px",
             backgroundColor: "#00aa00",
             color: "#ffffff",
             padding: { x: 20, y: 10 }
         });
 
-        startBtn.setInteractive();
+        startBtn.setInteractive({ useHandCursor: true });
 
         startBtn.on("pointerdown", () => {
             console.log("BUTTON WORKS");
